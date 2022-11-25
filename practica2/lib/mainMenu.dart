71958f9efc1acf8eddbd8ff66c11cd68cbe532ad
BenditaCompostela <sergio13por51@gmail.com>
2022-11-25 19:51:09 +0100

    alinear abajo

--- practica2/lib/mainMenu.dart
+++ practica2/lib/mainMenu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practica2/dificultad.dart';
 
 class mainMenu extends StatefulWidget{
   mainMenu({super.key});
@@ -11,23 +12,40 @@ class _mainMenuState extends State<mainMenu>{
   @override
   Widget build(BuildContext context){
     return Scaffold(//estructura pantalla: AppBar y body
-      body: body(),
+      body: body(context),
       );
   }
 }
 
-Widget body(){
+Widget body(BuildContext context){
   return Container(
      decoration: BoxDecoration(
       image: DecorationImage(image: AssetImage('assets/fondomenuinicial.png'),
-          fit: BoxFit.contain
+          fit: BoxFit.cover
       )
     ),
     child: Center(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          Image.asset('assets/play.png'),
-          Image.asset('assets/exit.png')
+          TextButton(
+              onPressed: () {
+                /*Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MainGame()),
+            );*/
+              },
+              child: Image.asset('assets/play.png'),
+            ),
+          TextButton(
+              onPressed: () {
+                /*Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ()),
+            );*/
+              },
+              child: Image.asset('assets/exit.png'),
+            ),
         ],
       ),
     ),
