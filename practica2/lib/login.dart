9f74515d8b9bc09879630b9fa18db423ae7213a8
reinachikita <violetatrujilloalmeida@gmail.com>
2022-11-26 16:15:55 +0100

    a

--- practica2/lib/login.dart
+++ practica2/lib/login.dart
@@ -1,8 +1,16 @@
 
 
-import 'package:flutter/material.dart';
 
+
+import 'package:flutter/material.dart';
+import 'package:practica2/mainMenu.dart';
+import 'package:flutter/services.dart';
+
+
+bool go=false;
 class login extends StatefulWidget{
+
+
   login({super.key});
 
   @override
@@ -10,17 +18,19 @@ class login extends StatefulWidget{
 }
 
 class _loginState extends State<login>{
+
+
   @override
   Widget build(BuildContext context){
     return Scaffold(//estructura pantalla: AppBar y body
-      body: body(),
+      body: body(context),
 
       );
 
   }
 }
 
-Widget body(){
+Widget body(BuildContext context){
   return  Container(
     decoration: BoxDecoration(
       image: DecorationImage(image: AssetImage('assets/Mesa de trabajo 1.png'),
@@ -32,12 +42,12 @@ Widget body(){
 
       child: Center(
         child: Column(
-          mainAxisAlignment :MainAxisAlignment.spaceBetween,
+          mainAxisAlignment :MainAxisAlignment.center,
 
           children: <Widget>[
 
-            usernameSpace(),
-            botonContinuar(),
+            usernameSpace(context),
+            botonContinuar(context),
           ],
 
       ),
@@ -62,36 +72,52 @@ Widget body(){
 
 }*/
 
-Widget usernameSpace(){
+Widget usernameSpace(BuildContext context){
   return Container(
-padding: EdgeInsets.only(left: 20,right: 20,bottom:0),
-      child: TextField(
-        textAlign: TextAlign.center,
+padding: EdgeInsets.only(left: 90,right: 90,top:383),
+      child: TextFormField(
+
+        textAlign: TextAlign.start,
+
+          keyboardType: TextInputType.name,
+
+
+    style: TextStyle(
+      fontFamily: 'Counter new'
+    ),
+          maxLength: 10,
+
           decoration: InputDecoration(
-              hintText: "Usuario",
-              fillColor: Colors.white30,
+
+              hintText: "Usuario *",
+              fillColor: Colors.black,
               filled: false
           )
+
       ),
   );
 
 }
 
-Widget botonContinuar(){
+Widget botonContinuar(BuildContext context){
 
   return Container(
 
-padding: EdgeInsets.only(top:0,left: 250),
+padding: EdgeInsets.only(top:0,left: 300),
     child: IconButton(
       icon:  Image.asset('assets/flecha_login.png'),
-      iconSize: 100.0,
+      iconSize: 150.0,
+
       onPressed: () {
 
-        //Navigator.push(
-        // context,
-        //   MaterialPageRoute(builder: (context) => login()),
-        // );
-      },
+  Navigator.push(
+    context,
+
+
+    MaterialPageRoute(builder: (context) => mainMenu()),
+  );
+}
+
 
       ),
 
@@ -103,10 +129,6 @@ padding: EdgeInsets.only(top:0,left: 250),
 
 
 }
-
-
-
-
 
 
 
