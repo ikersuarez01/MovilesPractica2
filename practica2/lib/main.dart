82f6b6cf9f37cad2ca51b7877644b46971de7ab4
BenditaCompostela <sergio13por51@gmail.com>
2022-11-29 13:23:25 +0100

    sonido

--- practica2/lib/main.dart
+++ practica2/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:practica2/login.dart';
 import 'package:practica2/mainMenu.dart';
-//import 'package:audioplayer/audioplayer.dart';
+import 'package:audioplayer/audioplayer.dart';
 
 //PANTALLA DE INICIO
 void main() {
@@ -40,7 +40,7 @@ class _InicioState extends State<Inicio>{
 }
 
 Widget portada(BuildContext context){
-    //final audio = AudioPlayer();
+    final audio = AudioPlayer();
   return Container(
     decoration: BoxDecoration(
       image: DecorationImage(image: AssetImage('assets/fondoinicio.png'), //aquí va la imagen de fondo
@@ -62,7 +62,7 @@ Widget portada(BuildContext context){
             ),
             TextButton(
               onPressed: () {
-                //audio.play('assets/musica.mp3');
+                audio.play('assets/musica.mp3');
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => mainMenu()),
